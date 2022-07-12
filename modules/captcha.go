ddf8169de54aec245f751596206581fd92c4c538
theolsdev <theobad55@gmail.com>
2022-07-12 18:13:56 +0200

    Add Send Captcha Function + Comments

--- modules/captcha.go
+++ modules/captcha.go
@@ -1,18 +1,21 @@
 package modules
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
 	uuid "github.com/nu7hatch/gouuid"
 )
 
+/** Session Structure */
 type CaptchaSession struct {
 	UUID   string       `json:"uuid"`
 	Client *http.Client `json:"client"`
 	Key    string       `json:"key"`
 }
 
+/** Init Session **/
 func (s *CaptchaSession) InitSession(key string) {
 	u, _ := uuid.NewV4()
 	s.UUID = u.String()
@@ -21,4 +24,9 @@ func (s *CaptchaSession) InitSession(key string) {
 		Transport: &http.Transport{},
 		Timeout:   5 * time.Second,
 	}
+}
+
+/** Send Captcha Method */
+func (s *CaptchaSession) SendCaptcha() error {
+	return errors.New("Error_Untype")
 }
